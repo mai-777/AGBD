75d1dfb028b67eb81228aeb709de77acb81fb6f6
Mailen Escobar <mailenescobar5d@gmail.com>
2024-05-14 15:36:32 -0300

    Ejercicio 1 hecho, mostrar ciudades(sin repetirse) en orden alfabetico

---
+++ EVALUACION/Consignas_Evaluacion.sql
@@ -0,0 +1,41 @@
+-- 1) Mostrar todas las ciudades donde se 
+-- reportaron asesinatos. 
+-- Las ciudades deben estar en orden alfabético
+-- y no debe haber repetidos
+
+-- 2) Listar el nombre y los datos del auto
+-- (marca, modelo y patente) de todos los
+-- varones más de 70 años, elegir el orden
+-- en que se muestran 
+
+-- 3) Buscar la lista de todas las personas
+-- que no tienen licencia de conducir
+
+-- 4) Encontrar el nombre y la dirección completa
+-- de la persona que vive en la anteúltima casa
+-- de la calle 'Franklin Ave'
+
+-- 5) Encontrar la edad mínima, máxima y promedio
+-- de las mujeres que tienen licencia de conducir
+
+-- 6) Contar la cantidad de miembros del 
+-- gimnasio de cada categoría (gold, silver, 
+-- etc), ordenadas desde la que tiene más 
+-- miembros a la que tiene menos
+
+-- 7) Mostrar los nombres, numeros de seguridad social
+-- (ssn), id de membresía y tipo de membresía de
+-- las personas que fueron al gimnasio aunque sea una
+-- vez durante el 2018. Los resultados deben estar
+-- ordenados de manera que los de un mismo tipo de
+-- membresía (gold, silver, etc) aparezcan todos 
+-- seguidos y dentro de cada una ordenados por ssn.   
+
+-- 8) Hacer un ranking de los autos 
+-- (marca y modelo) que manejan las
+-- personas que ganan menos de 35000 dolares
+-- al año.  Este ranking debe estar ordenado
+-- desde los más populares a los menos populares
+-- y no debe incluir los modelos que tengan 
+-- menos de 4 autos
+
---
+++ EVALUACION/Ejercicio1.sql
@@ -0,0 +1,2 @@
+SELECT DISTINCT city FROM crime_scene_report
+ORDER BY city ASC
