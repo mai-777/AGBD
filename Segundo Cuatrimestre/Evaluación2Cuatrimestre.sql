5ab9942f6ecac8e25c7afda697048e6c4e898ea3
Mailen Escobar <mailenescobar5d@gmal.com>
2024-06-10 12:11:25 -0300

    Evaluacion segundo Cuatrimestre me quede en el punto 2

---
+++ Segundo Cuatrimestre/Evaluación2Cuatrimestre.sql
@@ -0,0 +1,38 @@
+/*no hacer 1 y 2*/
+/*3*/UPDATE employees SET Fax = Phone, 
+                          Phone = Fax
+
+/*4(no termnadp)*/DELETE FROM playlist_track 
+WHERE TrackId in (
+	SELECT TrackId FROM tracks 
+	GROUP by GenreId
+	HAVING count(TrackId) < 50);
+
+DELETE FROM invoice_items 
+WHERE TrackId in (
+	SELECT TrackId FROM tracks 
+	GROUP by GenreId
+	HAVING count(TrackId) < 50);
+	
+DELETE FROM tracks 
+WHERE TrackId in (
+	SELECT TrackId FROM tracks 
+	GROUP by GenreId
+	HAVING count(TrackId) < 50);
+
+DELETE FROM genres 
+WHERE GenreId in (
+	SELECT GenreId FROM tracks 
+	GROUP by GenreId
+	HAVING count(TrackId) < 50);
+/*opcion2(no terminado)*/UPDATE tracks SET GenreId = NULL
+WHERE GenreId IN (SELECT TrackId FROM tracks 
+	GROUP by GenreId
+	HAVING count(TrackId) < 50)
+	
+DELETE FROM genres 
+WHERE GenreId in (
+	SELECT GenreId FROM genres g
+	LEFT JOIN tracks t ON g.GenreId = t.GenreId
+	GROUP by GenreId
+	HAVING count(TrackId) < 50); 
